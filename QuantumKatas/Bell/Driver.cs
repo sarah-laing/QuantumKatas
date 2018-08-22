77022c8096cf7314e2ceaa55793700971ea83621
Sarah Laing <sarah.laing.dev@gmail.com>
2018-08-21 22:40:15 -0700

    test commit.

--- QuantumKatas/Bell/Driver.cs
+++ QuantumKatas/Bell/Driver.cs
@@ -17,7 +17,7 @@ namespace Quantum.Bell
                     System.Console.WriteLine($"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4}");
                 }
             }
-            System.Console.WriteLine("Press key...");
+            System.Console.WriteLine("Press a key...");
             System.Console.ReadKey();
         }
     }
